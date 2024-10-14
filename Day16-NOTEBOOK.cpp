1740d60f3a24c382315c3eaa2b5a5bd648c7078b
Shriniwas Mourya <145322480+shriniwasmourya@users.noreply.github.com>
2024-10-13 22:13:48 -0400

    Add files via upload

---
+++ Day16-NOTEBOOK.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;  
+
+    while (T--) {
+        int N;
+        cin >> N;
+        cout<<N*10<<endl;
+        
+        
+      
+    }
+
+    return 0;
+}
