dffb72d1319fe4c2805cc9c617b8cf9a1604251d
Shriniwas Mourya <145322480+shriniwasmourya@users.noreply.github.com>
2024-10-20 12:05:28 -0400

    Add files via upload

---
+++ Day18-BalancedReversal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        string A;
+        cin >> A;
+        
+        int count0 = 0, count1 = 0;
+        for (char c : A) {
+            if (c == '0') count0++;
+            else count1++;
+        }
+        
+        string result = string(count0, '0') + string(count1, '1');
+        cout << result << endl;
+    }
+    return 0;
+}
