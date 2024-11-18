a2be628d6581390a5bdfb03223042f7741aa01ba
Shriniwas Mourya <145322480+shriniwasmourya@users.noreply.github.com>
2024-11-18 10:50:35 -0500

    Add files via upload

---
+++ Day47-JENGA.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+        if (X % N == 0)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
